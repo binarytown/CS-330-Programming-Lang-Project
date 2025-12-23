3ba5e5ef52c98a5be4a2fb27c1370a2e2c26242f
Tuba Siddiqui <tubaashmeera@gmail.com>
2025-12-22 19:16:47 -0500

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include<iostream> // this line is a pre-processor, it allows us to perform our operations
+using namespace std;
+
+int main(){ // to return int data types
+    cout << "Hello World!" << endl;       // to help us print our desired texts or sentenses
+    return 0;
+} 
+
